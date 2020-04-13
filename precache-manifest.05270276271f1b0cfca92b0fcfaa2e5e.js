45b1254c8b199e522fe13d8e4571a3f1915bcaf7
lior.derei <lior.derei@ezbob.com>
2020-04-13 15:45:43 +0300

    Updates

---
+++ precache-manifest.05270276271f1b0cfca92b0fcfaa2e5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4e54d7f9aa019d5ef5a127fd182ad27",
+    "url": "/liorderei/index.html"
+  },
+  {
+    "revision": "109348daf986b3404069",
+    "url": "/liorderei/static/css/main.38af8b1f.chunk.css"
+  },
+  {
+    "revision": "872ee35148d33d167f3e",
+    "url": "/liorderei/static/js/2.89904b5f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/liorderei/static/js/2.89904b5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "109348daf986b3404069",
+    "url": "/liorderei/static/js/main.5f09b5df.chunk.js"
+  },
+  {
+    "revision": "024a2e5cb2cfa55e7a7e",
+    "url": "/liorderei/static/js/runtime-main.189f9f8f.js"
+  }
+]);
